cb59786d48e636a0bd38b63c7528ff3fbbc1432e
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-06-27 19:55:39 +0100

    stored html elements in variables

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+const cells = document.querySelectorAll('.cell');
+const player1Input = document.getElementById('player1-input');
+const player2Input = document.getElementById('player2-input');
+const startButton = document.getElementById('start-btn');
+const resetButton = document.getElementById('reset-btn');
+const messageElement = document.getElementById('message');
+
