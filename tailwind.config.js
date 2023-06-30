00331f83abab34009620e8d01589daa6ecfe6791
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-06-30 19:56:04 +0100

    gh pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html}", "./dist/*.{js}"],
+  content: ["*.{html}", "./dist/*.{js,css}"],
   theme: {
     extend: {},
   },
