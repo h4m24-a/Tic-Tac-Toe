1371e5e8312277f201b39d9cf4a550e8beab5120
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-06-30 16:58:03 +0100

    creating startGame function - what happens when a user clicks the start game button

--- dist/app.js
+++ dist/app.js
@@ -5,3 +5,38 @@ const startButton = document.getElementById('start-btn');
 const resetButton = document.getElementById('reset-btn');
 const messageElement = document.getElementById('message');
 
+
+startButton.addEventListener("click", startGame)
+resetButton.addEventListener("click", resetNames)
+
+
+let currentPlayer = "";
+let gameOver = true;
+
+
+function startGame() {
+  const player1Name = player1Input.value || 'Player 1';
+  const player2Name = player2Input.value || 'Player 2';
+
+  player1Input.disabled = true;
+  player2Input.disabled = true;
+  startButton.textContent = "Restart Game"
+
+  currentPlayer = "X"
+  gameOver = false;
+
+  cells.forEach(cell => {
+    cell.textContent=""
+    cell.classList.remove("bg-green-300")
+    cell.classList.add("hover:bg=gray-200")
+    cell.addEventListener("click", handleCellClick)
+  })
+
+  displayMessage(`Current Turn:${player1Name}`)
+
+}
+
+
+function displayMessage(message) {
+  messageElement.textContent = message
+}
