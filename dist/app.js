a82bed6e2b6615f28a84bb6d8a6e10ee60e633c1
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-06-30 17:16:45 +0100

    added the handleCellClick and checkForWin functions - both handle the logic for when a cell is clicked and how a player wins

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ startButton.addEventListener("click", startGame)
 resetButton.addEventListener("click", resetNames)
 
 
-let currentPlayer = "";
+let currentPlayer = '';
 let gameOver = true;
 
 
@@ -26,7 +26,7 @@ function startGame() {
   gameOver = false;
 
   cells.forEach(cell => {
-    cell.textContent=""
+    cell.textContent=''
     cell.classList.remove("bg-green-300")
     cell.classList.add("hover:bg=gray-200")
     cell.addEventListener("click", handleCellClick)
@@ -39,4 +39,34 @@ function startGame() {
 
 function displayMessage(message) {
   messageElement.textContent = message
+}
+
+
+function handleCellClick() {
+  if (gameOver) return;
+
+  if (this.textContent !== '') {
+    // Cell already filled
+    return;
+  }
+
+  this.textContent = currentPlayer;
+  this.classList.remove('hover:bg-gray-200');
+  this.removeEventListener('click', handleCellClick);
+
+  if (checkForWin()) {
+    // Game over, current player wins
+    const currentPlayerName = currentPlayer === 'X' ? player1Input.value || 'Player 1' : player2Input.value || 'Player 2';
+    displayMessage(`Congratulations ${currentPlayerName}! You Win!`);
+    gameOver = true;
+  } else if (checkForDraw()) {
+    // Game over, it's a draw
+    displayMessage("It's a draw!");
+    gameOver = true;
+  } else {
+    // Switch players
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    const currentPlayerName = currentPlayer === 'X' ? player1Input.value || 'Player 1' : player2Input.value || 'Player 2';
+    displayMessage(`Current Turn: ${currentPlayerName}`);
+  }
 }
